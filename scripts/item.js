8195687dd9ca72b11d1cbcd78d041fdb1482c299
Jon <jonathanjackson424@gmail.com>
2019-10-30 12:37:09 -0400

    try

--- scripts/item.js
+++ scripts/item.js
@@ -1,15 +1,19 @@
 function validateName(name){
-  if (name === undefined){
-   throw new TypeError ('Name must not be blank');
+    try{
+      if (name === undefined){
+      throw new TypeError ('Name must not be blank');
+    } catch(e){
+      console.log(e.message);
+    }
   }
 }
 
 
 function create(name){
   return{
-    id,
+    id: cuid(),
     name,
-    checked,
+    checked: false,
   };
 }
 
