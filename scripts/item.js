fb40c9c1f5251d9887b464dcaf45c46c82e3eba8
Jon <jonathanjackson424@gmail.com>
2019-10-30 12:31:11 -0400

    new factory function

--- scripts/item.js
+++ scripts/item.js
@@ -1,1 +1,19 @@
-export default {};
+function validateName(name){
+  if (name === undefined){
+   throw new TypeError ('Name must not be blank');
+  }
+}
+
+
+function create(name){
+  return{
+    id,
+    name,
+    checked,
+  };
+}
+
+export default {
+  validateName,
+  create,
+};
